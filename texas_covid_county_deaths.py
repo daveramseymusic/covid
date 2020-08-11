6f1876f78aaaf8a93deb8327d68145e506bac174
daveramseymusic <63751809+daveramseymusic@users.noreply.github.com>
2020-08-11 17:45:34 -0400

    clean up df for future stuff

--- texas_covid_county_deaths.py
+++ texas_covid_county_deaths.py
@@ -17,103 +17,128 @@ data_xls = pd.read_excel('test.xlsx')
 data_xls.to_csv('new_covid_test.csv', encoding='utf-8')
 c_cov = pd.read_csv('new_covid_test.csv')
 
-##pivot table so the columns ar county name 
-c_cov = c_cov.set_index('Unnamed: 0').T
-# c_cov.reset_index()
+# ##pivot table so the columns ar county name 
+# c_cov = c_cov.set_index('Unnamed: 0').T
+# # c_cov.reset_index()
 
-#change "county name" to a date so the whole column can be in datetime
-c_cov.iloc[0,1] = '01/12/1981'    
+#shorten the publishing date info in c_cov
+#jeopardy.rename(columns=lambda x: x.strip(), inplace=True)
+c_cov.rename(columns=lambda x: x.replace('COVID-19 Total Fatalities by County, 3/7/2020 - ', ''), inplace=True)
+
+# change the 1 to "dates"
+c_cov.iloc[1,0] = str(c_cov.iloc[1,0])
+# c_cov.iloc[1,0] = 'dates'
+
+print(c_cov.iloc[:4,:2])
+
+#make the columns the right ones:
+new_header = c_cov.iloc[1] #grab the first row for the header
+c_cov = c_cov[2:] #take the data less the header row
+c_cov.columns = new_header #set the header row as the df header
+
+#set index
+c_cov = c_cov.iloc[:, 1:] #take the data less the first two columns or so
+
+c_cov = c_cov.set_index('County Name')
+# then transpose
+c_cov = c_cov.T
+# reset index
+c_cov = c_cov.reset_index()
+
+
 
 # After pivot & datetime clenup, change the cell above the dates from 'county name' to 'dates:
-c_cov.iloc[0,1] = 'dates'
 
-#clean county names and everything else in the headers
-c_cov.iloc[0,:] = c_cov.iloc[0,:].str.strip().str.lower().str.replace(' ', '_').str.replace('(', '').str.replace(')', '')
+# # parse into datetime and shortent to date()
+c_cov.iloc[:,0] = c_cov.iloc[:,0].apply(lambda x: parse(x).date())
 
-#create the column names as I prefer them starting on the second row & reset index:
-c_cov.columns = c_cov.iloc[0]
-c_cov.reset_index()
+#make all the column names aka headers strings (cause the first one wasnt for like NO REASON)
+c_cov.columns = [str(i) for i in c_cov.columns.values.tolist()]
+c_cov.rename(columns=lambda x: x.replace('1', 'dates'), inplace=True)  
 
+#clean county names and everything else that will be column headers
+c_cov.rename(columns=lambda x: x.strip()\
+                                .lower().replace(' ', '_').replace('(', '').replace(')', '')\
+                                , inplace=True)
 
-# parse into datetime and shortent to date()
-c_cov.iloc[1:,1] = c_cov.iloc[1:,1].apply(lambda x: parse(x).date())
-
-# # filter out everything but tarrant county
+# # # filter out everything but tarrant county
 tarrant_df = c_cov[[ 'dates', 'tarrant']].reset_index()
 
-#make data integers:
-tarrant_df.iloc[1:,2] = pd.to_numeric(tarrant_df.iloc[1:,2], downcast='integer')
+print(tarrant_df.head())
 
-#variable for today's deaths
-today_deaths = tarrant_df.iloc[-1,2]
+# #make data integers:
+# tarrant_df.iloc[1:,2] = pd.to_numeric(tarrant_df.iloc[1:,2], downcast='integer')
 
-#to get deaths each day you must subtract today from 8 days ago.  making 8 days the zero
-week_ago_deaths = tarrant_df.iloc[-8,2]
+# #variable for today's deaths
+# today_deaths = tarrant_df.iloc[-1,2]
 
-two_week_ago_deaths = tarrant_df.iloc[-15,2]
+# #to get deaths each day you must subtract today from 8 days ago.  making 8 days the zero
+# week_ago_deaths = tarrant_df.iloc[-8,2]
 
-deaths_last_seven_days = today_deaths - week_ago_deaths
-deaths_week_before = week_ago_deaths - two_week_ago_deaths
+# two_week_ago_deaths = tarrant_df.iloc[-15,2]
 
-# #make most recent column name and 1 week ago column name as variables to print out
+# deaths_last_seven_days = today_deaths - week_ago_deaths
+# deaths_week_before = week_ago_deaths - two_week_ago_deaths
 
-most_recent_date = tarrant_df.dates.iloc[-1]
-two_week_ago_date = tarrant_df.iloc[-8,1]
-county = (tarrant_df.iloc[0,2])
+# # #make most recent column name and 1 week ago column name as variables to print out
 
-# #create average deaths per day for each week:
-ave_deaths_last_seven_days = deaths_last_seven_days / 7
-ave_deaths_week_before = deaths_week_before / 7
+# most_recent_date = tarrant_df.dates.iloc[-1]
+# two_week_ago_date = tarrant_df.iloc[-8,1]
+# county = (tarrant_df.iloc[0,2])
 
-#### stuff befor graffing
-# # print total deaths
-# print('\n' + county +' county' + '\n''\n' + '  Total Deaths:' + '\n')
-# print('Last 7 days' +'\n' '(ending ' + str(most_recent_date) + '): ' + str(deaths_last_seven_days))
-# print('week before' +'\n'+ '(ending ' + str(two_week_ago_date) + '): ' + str(deaths_week_before))
+# # #create average deaths per day for each week:
+# ave_deaths_last_seven_days = deaths_last_seven_days / 7
+# ave_deaths_week_before = deaths_week_before / 7
 
-# #print ave deaths per day
-# print('\n' + '  Ave Deaths/day:' + '\n')
-# print('Last 7 days' +'\n' '(ending ' + str(most_recent_date) + '): ' + str(ave_deaths_last_seven_days))
-# print('week before' +'\n'+ '(ending ' + str(two_week_ago_date) + '): ' + str(ave_deaths_week_before))
+# #### stuff befor graffing
+# # # print total deaths
+# # print('\n' + county +' county' + '\n''\n' + '  Total Deaths:' + '\n')
+# # print('Last 7 days' +'\n' '(ending ' + str(most_recent_date) + '): ' + str(deaths_last_seven_days))
+# # print('week before' +'\n'+ '(ending ' + str(two_week_ago_date) + '): ' + str(deaths_week_before))
 
-# # create a function that finds the last day a death was reported:
-# recent_repeated_data = tarrant_df[tarrant_df.tarrant == today_deaths]
-# # one_day = datetime.timedelta(days=1)
-# date_most_recent_update = recent_repeated_data.dates.iloc[0]
-# days_since_update = date_most_recent_update - most_recent_date - timedelta(days=1)
+# # #print ave deaths per day
+# # print('\n' + '  Ave Deaths/day:' + '\n')
+# # print('Last 7 days' +'\n' '(ending ' + str(most_recent_date) + '): ' + str(ave_deaths_last_seven_days))
+# # print('week before' +'\n'+ '(ending ' + str(two_week_ago_date) + '): ' + str(ave_deaths_week_before))
 
-# #print date of last reported death:
-# print('last day there was a change in deaths reported: ' + str(date_most_recent_update))
-# print('number of days since last update:   ' + str(days_since_update))
+# # # create a function that finds the last day a death was reported:
+# # recent_repeated_data = tarrant_df[tarrant_df.tarrant == today_deaths]
+# # # one_day = datetime.timedelta(days=1)
+# # date_most_recent_update = recent_repeated_data.dates.iloc[0]
+# # days_since_update = date_most_recent_update - most_recent_date - timedelta(days=1)
 
-
-# print(tarrant_df.iloc[-15:,1:])
-
-# #find the last day deaths actually changed. then save the last day to a variable.
-# most_recent_updated_death_total_df = tarrant_df.loc[(tarrant_df.dates == date_most_recent_update), ['tarrant']]
-# most_recent_updated_death_total = most_recent_updated_death_total_df.iloc[0,0]
-# print(most_recent_updated_death_total)
-
-# week_before_mrud_total_df = tarrant_df.loc[(tarrant_df.dates == date_most_recent_update - timedelta(days=8)), ['tarrant']]
-# week_before_mrud_total = week_before_mrud_total_df.iloc[0,0]
-# print(week_before_mrud_total)
+# # #print date of last reported death:
+# # print('last day there was a change in deaths reported: ' + str(date_most_recent_update))
+# # print('number of days since last update:   ' + str(days_since_update))
 
 
-# updated_last_seven_ave = (most_recent_updated_death_total - week_before_mrud_total) / 7
-# print(updated_last_seven_ave)
+# # print(tarrant_df.iloc[-15:,1:])
+
+# # #find the last day deaths actually changed. then save the last day to a variable.
+# # most_recent_updated_death_total_df = tarrant_df.loc[(tarrant_df.dates == date_most_recent_update), ['tarrant']]
+# # most_recent_updated_death_total = most_recent_updated_death_total_df.iloc[0,0]
+# # print(most_recent_updated_death_total)
+
+# # week_before_mrud_total_df = tarrant_df.loc[(tarrant_df.dates == date_most_recent_update - timedelta(days=8)), ['tarrant']]
+# # week_before_mrud_total = week_before_mrud_total_df.iloc[0,0]
+# # print(week_before_mrud_total)
+
+
+# # updated_last_seven_ave = (most_recent_updated_death_total - week_before_mrud_total) / 7
+# # print(updated_last_seven_ave)
 
 
 
-#print bar graph of Tarrant County Deaths
-fig, ax = plt.subplots(figsize=(10, 6))
-ax.xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))
-ax.bar(tarrant_df.iloc[-(len(tarrant_df.dates)-1):,1], tarrant_df.iloc[-(len(tarrant_df.dates)-1):,2], width=0.8, align='center')
-plt.xlabel('Dates')
-plt.ylabel('Deaths')
-plt.title('Tarrant County Total Covid Deaths over Time')
-plt.show()
+# #print bar graph of Tarrant County Deaths
+# fig, ax = plt.subplots(figsize=(10, 6))
+# ax.xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))
+# ax.bar(tarrant_df.iloc[-(len(tarrant_df.dates)-1):,1], tarrant_df.iloc[-(len(tarrant_df.dates)-1):,2], width=0.8, align='center')
+# plt.xlabel('Dates')
+# plt.ylabel('Deaths')
+# plt.title('Tarrant County Total Covid Deaths over Time')
+# plt.show()
 
-
+# ###
 
 ##figure out daily deaths to watch rate:
 #set y_values to equal last 7 days
